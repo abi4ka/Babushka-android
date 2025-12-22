6724592d92adcce8d43b5057d521f15cc1fc95c8
cmarinoa <cmarinoa25@fpcoruna.afundacion.org>
2025-12-22 20:19:45 +0100

    Creado XML activity_register a base de cambiar algunas cosas del activity_login.xml + creado el esqueleto de RegisterActivity.java

--- app/src/main/java/com/example/babushka/RegisterActivity.java
+++ app/src/main/java/com/example/babushka/RegisterActivity.java
@@ -1,24 +1,62 @@
 package com.example.babushka;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etUsuario;
+    private EditText etClave;
+    private Button btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_register); // tu XML
+
+        // Conectamos con botones del xml
+        etUsuario = findViewById(R.id.etUsuario);
+        etClave = findViewById(R.id.etClave);
+        btnRegistrarse = findViewById(R.id.btnRegistrarse);
+
+        // Acción del botón
+        btnRegistrarse.setOnClickListener(v -> registrarUsuario());
+    }
+
+    private void registrarUsuario() {
+
+        String usuario = etUsuario.getText().toString().trim();
+        String clave = etClave.getText().toString().trim();
+
+        // Validaciones
+        if (usuario.isEmpty() || clave.isEmpty()) {
+            Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Aquí llamaremos al API
+
+
+        /*
+        1. Enviar usuario y clave al servidor
+        2. Esperar respuesta
+        3. Si es correcto → volver al login
+        4. Si hay error → mostrar mensaje
+        */
+
+        // Simulación de registro correcto
+        Toast.makeText(this, "Registro completado", Toast.LENGTH_SHORT).show();
+
+        volverALogin();
+    }
+
+    private void volverALogin() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish(); // cerramos esta pantalla
     }
 }
