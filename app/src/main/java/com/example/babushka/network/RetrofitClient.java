97e95c8736df9e329832fe96a9c8bbb119de12d1
abik <nikita161787@gmail.com>
2026-01-09 08:52:47 +0100

    server connection change

--- app/src/main/java/com/example/babushka/network/RetrofitClient.java
+++ app/src/main/java/com/example/babushka/network/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://47.59.1.79:25565";
+    private static final String BASE_URL = "http://82.26.150.189:25569";
 
     private static Retrofit retrofit;
 
