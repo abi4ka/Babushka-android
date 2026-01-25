1a1068d1a68f2f515f15c35565ab1c3f595c6d78
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-01-25 12:56:46 +0100

    Update RecipeApi.java

--- app/src/main/java/com/example/babushka/network/RecipeApi.java
+++ app/src/main/java/com/example/babushka/network/RecipeApi.java
@@ -4,19 +4,67 @@ import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RecipeApi {
-
-    @GET("/recipe/get-page")
-    Call<List<RecipeResponseDto>> getRecipes(
+    @GET("/recipes")
+    Call<ClientResponse<List<RecipeResponseDto>>> getRecipes(
             @Query("page") int page,
             @Query("size") int size,
-            @Query("search") String search
-    );
+            @Query("search") String search,
+            @Query("userIdFav") long userIdFav);
 
-    @GET("/recipe/{id}/get-image")
-    Call<ResponseBody> getRecipeImage(@Path("id") long id);
+    @GET("/recipes/{id}/image")
+    Call<ResponseBody> getRecipeImage(
+            @Path("id") long id);
+
+    @GET("/recipes/user/{userId}")
+    Call<ClientResponse<List<RecipeResponseDto>>> getMyRecipes(
+            @Path("userId") long userId,
+            @Query("page") int page,
+            @Query("size") int size,
+            @Query("userIdFav") long userIdFav);
+
+    @GET("/recipes/favorite/user/{userId}")
+    Call<ClientResponse<List<RecipeResponseDto>>> getFavoriteRecipes(
+            @Path("userId") long userId,
+            @Query("page") int page,
+            @Query("size") int size,
+            @Query("userIdFav") long userIdFav);
+
+    @GET("/users/{userId}/info")
+    Call<ClientResponse<UserInfoDto>> getUserInfo(
+            @Path("userId") long userId);
+
+    @POST("/recipes/{id}/favorite")
+    Call<ClientResponse> postFavoriteRecipes(
+            @Path("id") long recipeId,
+            @Query("userId") long userId,
+            @Query("favorite") boolean favorite);
+
+    /*Este metodo hace una petición HTTP POST a la ruta /sessions del backend de Django,
+    que es la que maneja el login.
+    - Call <LoginResponseDto> es el tipo de respuesta que se recibe. "Call" se usa para manejar llamadas HTTP
+    asíncronas.
+    - LoginResponseDto es la clase que hemos creado que mapea el JSON que devuelve Django, en este caso, solo el token.
+    - login(@Body UserDto user) define el metodo login que acepta un objeto UserDto como cuerpo de la petición.
+    - @Body indica que Retrofit tomará ese objeto y lo convertirá a JSON automáticamente.
+    - UserDto es el Dto que hemos creado para el usuario.
+    */
+
+    @POST("/sessions")
+    Call<LoginResponseDto> login(@Body UserDto user);
+
+    /*Este metodo hace una petición HTTP POST a la ruta /users del backend de Django,
+  que es la que maneja el registro.
+  - Call <ResponseBody> es el tipo de respuesta que se recibe.
+  - ResponseBody es una clase genérica que recibe el cuerpo de la respuesta tal cual.
+  - login(@Body UserDto user) define el metodo register que acepta un objeto UserDto como cuerpo de la petición.
+  */
+    @POST("/users")
+    Call<ResponseBody> register(@Body UserDto user);
 }
