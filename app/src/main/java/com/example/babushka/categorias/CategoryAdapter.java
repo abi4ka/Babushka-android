cb94b9b38e794c75ff6ff86506af78275acb9d42
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-01-31 21:58:20 +0100

    Refactor name item_layout_categoria

--- app/src/main/java/com/example/babushka/categorias/CategoryAdapter.java
+++ app/src/main/java/com/example/babushka/categorias/CategoryAdapter.java
@@ -52,7 +52,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_layout, parent, false);
+                .inflate(R.layout.item_layout_categoria, parent, false);
         return new ViewHolder(v);
     }
 
